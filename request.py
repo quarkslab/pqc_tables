d93e06df7c57c458acc606a990499894ccd50d27
Laurent Grémy <lgremy@quarkslab.com>
2022-06-30 16:37:34 +0200

    Add a precision on SPHINCS+.

--- request.py
+++ request.py
@@ -22,51 +22,75 @@ def format_number(i):
         return "{:,}".format(int(i))
     return i
 
-filer = open("db.json")
+# key can be: sk, pk, sig, ct, ss
+def format_doc_code_data(variant, key):
+    keycode = key + "code"
+    if key in variant and keycode in variant :
+        return format_number(variant[key]) + " / " + format_number(variant[keycode])
+    elif key in variant:
+        return format_number(variant[key])
+    else:
+        return format_number(variant[keycode])
 
-try:
-    data = json.load(filer)
+def format_level(scheme):
+    return ", ".join([roman_number(i["level"]) for i in scheme["levels"]])
+
+# def simple_tables(data):
+    # names = [scheme["name"] for scheme in data]
+    # lname = max([len(i) for i in names])
+    # format_name = "{0: <" + str(lname) + "}"
+    # variants = []
+    # for scheme in data:
+        # var = []
+        # for variant in scheme["variants"]:
+            # if variant["levels"][-1]["level"] == "5":
+                # var.append(variant["variant"])
+        # variants.append(", ".join(var))
+    # lvariants = max([len(i) for i in variants])
+    # format_variants = "{0: <" + str(lvariants) + "}"
+
+    # for i in range(len(names)):
+        # print("| " + format_name.format(names[i]) + " | " + format_variants.format(variants[i]) + " |")
+
+def complete_tables(data):
     for scheme in data:
         for variant in scheme["variants"]:
             s = ""
-            level = ", ".join([roman_number(i["level"]) for i in scheme["levels"]])
-            pk = ""
-            if "pkcode" not in variant:
-                pk = format_number(variant["pk"])
-            else:
-                if not "pk" in variant:
-                    pk = format_number(variant["pkcode"])
-                else:
-                    pk = format_number(variant["pk"]) + " / " + format_number(variant["pkcode"])
             if scheme["scheme"] == "KEM":
                 s = "{0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} | {8} | {9}".format(
                         scheme["name"],
-                        level,
+                        format_level(scheme),
                         variant["variant"],
                         scheme["type"],
                         scheme["NIST"],
                         variant["ANSSI"],
-                        format_number(variant["sk"]),
-                        pk,
-                        format_number(variant["ct"]),
-                        format_number(variant["ss"]))
+                        format_doc_code_data(variant, "sk"),
+                        format_doc_code_data(variant, "pk"),
+                        format_doc_code_data(variant, "ct"),
+                        format_doc_code_data(variant, "ss"))
                 print(s)
+
             if scheme["scheme"] == "Signature":
                 sig = format_number(variant["sig"])
                 if "sigcode" in variant:
                     sig = sig + " / " + format_number(variant["sigcode"])
                 s = "{0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} | {8}".format(
                         scheme["name"],
-                        level,
+                        format_level(scheme),
                         variant["variant"],
                         scheme["type"],
                         scheme["NIST"],
                         variant["ANSSI"],
-                        format_number(variant["sk"]),
-                        pk,
-                        sig)
+                        format_doc_code_data(variant, "sk"),
+                        format_doc_code_data(variant, "pk"),
+                        format_doc_code_data(variant, "sig"))
                 print(s)
-                
+
+filer = open("db.json")
+
+try:
+    data = json.load(filer)
+    complete_tables(data)
 
 # Semantic error in db.json.
 except json.decoder.JSONDecodeError as e:
@@ -80,5 +104,4 @@ except Exception:
     filer.close()
     sys.exit(1)
 
-filer.close()
 sys.exit(0)
